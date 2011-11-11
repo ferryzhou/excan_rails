243a669fba89f960e3936e8d01955f9f2a07c062
Jin Zhou <ferryzhou@gmail.com>
2011-11-10 21:15:37 -0500

    delete unique migration

--- db/migrate/20111106135251_set_link_unique_to_post.rb
+++
@@ -1,9 +0,0 @@
-class SetLinkUniqueToPost < ActiveRecord::Migration
-  def self.up
-    add_index :posts, :link, :unique => true
-  end
-
-  def self.down
-    remove_index :posts, :column => :link
-  end
-end
